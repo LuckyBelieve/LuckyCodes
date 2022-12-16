9b6ee9bb3cb80ac5e180dc73bd6df975026a365b
Lucky <117139436+LuckyBelieve@users.noreply.github.com>
2022-12-16 20:42:22 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function toggle(id) {
+    let button=document.getElementById(id);
+    if(button.style.display=='block') {
+        button.style.display='none';
+    }else{
+        button.style.display='block';
+    }
+    let x="hello world";
+} 
+function myfunction(){
+    let a=23;b=453;
+    let  c=a+b;
+    // document.getelementbyId('demo').innerHTML="the sum is"+c+".";
+    document.getElementById('demo').innerHTML="the sum is "+c+".";
+}
+function hisfunction(){
+    let name='Believe';
+    document.getElementById('me').innerHTML="HELLO mr. "+name+" !!";
+    document.getElementById('you').innerHTML="Welcome to our website";
+}
+document.write(x);
