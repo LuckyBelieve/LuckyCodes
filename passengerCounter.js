f1dc858d57cf5614b04d35a5f19ab50d8a132103
Lucky <117139436+LuckyBelieve@users.noreply.github.com>
2022-12-29 14:41:39 +0200

    passengercounter

---
+++ passengerCounter.js
@@ -0,0 +1,33 @@
+
+const increBtn = document.querySelector('.increment-btn');
+// console.log(increBtn);
+
+let count = document.querySelector('.number-entered');
+// console.log(count);
+
+countEl = 0;
+
+function increment(){
+   countEl += 1;
+   count.innerText = countEl;
+}
+increBtn.addEventListener('click',increment);
+
+
+
+
+
+let div2 = document.querySelector('.buttons');
+let button = document.createElement('button');
+button.innerText = 'Remove Someone ';
+
+div2.append(button);
+button.setAttribute('class','increment-btn')
+
+// console.log(button);
+
+function decrement(){
+    countEl -=1;
+    count.innerText = countEl;
+}
+button.addEventListener('click',decrement);
