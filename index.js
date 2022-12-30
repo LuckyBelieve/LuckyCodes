40a6d6728b152ff8c43cfdbaba85a2b32f949002
Lucky <117139436+LuckyBelieve@users.noreply.github.com>
2022-12-30 22:45:03 +0200

    index.js functions

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 let myAge = 23;
 let myName = "Ineza lucky believe";
-console.log(myName);
+console.log( 'my name is '+ myName +' and my age is '+ myAge);
 let metime = "it's just a begining";
+
+function mynonVowel(word){
+
+    let nonVowelString = '';
+    for( let i=0;i<word.length;i++){
+        let myWord = word[i].toLowerCase();
+        if(myWord =='a' || myWord =='i' || myWord =='e' || myWord =='o' || myWord =='u' ){
+            continue;
+        }
+        nonVowelString+=word[i];
+    }
+    return nonVowelString;
+
+}
+console.log(mynonVowel('academy'));
